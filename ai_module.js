acb6b95eeaa34bb6d82f1d07c1ebdc2e64214c32
JesperZamora <jesp78m6@stud.kea.dk>
2024-02-29 23:50:41 +0100

    first commit

---
+++ ai_module.js
@@ -0,0 +1,32 @@
+const OpenAI = require("openai");
+const OPEN_API_KEY = process.env.MY_KEY
+const openai = new OpenAI({ apiKey: OPEN_API_KEY });
+
+const systemContent = 'You will make a quiz that revolves around the subjects date and year. You will generate 3 simple questions, with four possible solutions where only one i correct. The answers should be labeled A, B, C, D. I want the message back in an array and each questions and the four answers related to the question, should be in and object. Like [{question: "", A: "", B: "", C: "", D: "", correctAnswer: "", correctChoice: "", explanation: "" }]. The "correctAnswer" value, should be the value of either answer A, B, C or D. So if an answer has a date and year this should be the value of "correctAnswer. The correctChoice should be key of the correctAnswer. The user will give you one of five categories: Fashion, Animals, Programming, World, Games. When you recieve a category you generet the questions in regards to data and year subject and the category';
+
+let content = [{ role: "system", content: systemContent }];
+ 
+async function main(subject) {
+  try {
+    content.push({ role: "user", content: subject });
+    const completion = await openai.chat.completions.create({
+      messages: content,
+      model: "gpt-4",
+      /*temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,*/
+    });
+    console.log(completion);
+    
+    const completionResponse = completion.choices[0].message.content;
+    return completionResponse;
+
+  } catch(error) {
+    console.error("Error", error);
+    throw error;
+  }
+}
+
+module.exports = { main };
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const { main } = require("./ai_module");
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + "/public/quiz.html");
+  res.status(200).sendFile(__dirname +"/quiz_page.html");
+});
+
+app.get("/quiz/:subject", async (req, res) => {
+  try {
+    const quizSubject = req.params?.subject;
+
+    const JsonStringQuiz = await main(quizSubject);
+
+    const quizArray = JSON.parse(JsonStringQuiz);
+    console.log(quizArray);
+
+    return res.status(200).send({ data: quizArray });
+
+  } catch(error) {
+    return res.status(500).send("Internal Server Error");
+  }
+}); 
+
+app.listen(PORT, (error) => {
+  if(error) {
+    console.error("Error Starting Server", error);
+    return;
+  }
+  console.log("Server is running on", PORT);
+});
+
+/*TODO for the weekend (if there is time for it)
+  1. Make post endpoint that takes and object with the keys; time, score. Or use params.
+  2. The front end will take the score and time and send it to backend.
+  3. Data should be persisted to an array.
+  4. Send the array of objects back and display them in 'Score Board'.
+  5. Look into how to optimize the speed of quiz question generation. Maybe look into how the API streams works with Chat-GPT, see if can get quiz questions in a continuously stream instead?
+  6. Fix the answer buttons, so they are disabled once they are clicked and enabled when the next question is displayed.
+*/
